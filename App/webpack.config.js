f3a436b7c8e4590f347634fda08537dfb5f91db5
Andrei Positko <andrei.positko@chisw.com>
2020-08-12 18:23:20 +0300

    babel settings

--- App/webpack.config.js
+++ App/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     entry: ['./src/script.js'],
@@ -14,9 +15,20 @@ module.exports = {
         new HTMLPlugin({
             filename: 'index.html',
             template: './src/index.html'
-        })
+        }),
+        new ExtractTextPlugin('bundle.css')
     ],
     resolve: {
         extensions: ['.js']
-    }
+    },
+    module: {
+        rules: [
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+          { test: /\.css$/, loader: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              use: 'css-loader'
+          })
+        }
+        ]
+      }
 };
