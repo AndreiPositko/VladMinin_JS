3fdc856a4119589f45c62ddf63ac740690f2c862
PositkoAndrei <positkoandrei@gmail.com>
2020-08-31 18:21:54 +0300

    some changes in Basic JS

--- BasicJS/arraysAdditional/script.js
+++ BasicJS/arraysAdditional/script.js
@@ -8,8 +8,17 @@ let array = str.split(',');
 array.splice(1, 0, '11');
 
 let newArray = array.concat([1, 2]);
-// console.log(newArray);
+console.log('Concat - ', newArray);
 // console.log(array);
+
+//Concat Start
+let arr1 = [1, 2, 3, 4, 5, 6];
+let arr2 = ['a', 'b', 'c', 'd', 'e'];
+let newArr = [];
+newArr = newArr.concat(arr1, arr2);
+console.log('NewArray - ', newArr);
+//Concat End
+
 
 let objArr = [
     {name: 'Max', age: 27},
@@ -33,3 +42,56 @@ let numArr = array.map(function(i) {
 })
 console.log('Map', numArr);
 
+//Every Start
+let evArr1 = [54, 63, 5, 18, 45, 95, 38];
+let evArr2 = [122, 54, 84, 95, 231, 72, 96];
+
+function isBigger(element, index, array) {
+    return element >= 10;
+}
+console.log(evArr1.every(isBigger));
+console.log(evArr2.every(isBigger));
+//Every end
+
+//Fill start
+let fillArr = [54, 63, 5, 18, 45, 95, 38];
+console.log(fillArr.fill(7, 3));
+//Fill end
+
+//Filter start
+let filterArr = [54, 2, 5, 18, 45, 95, 38];
+let result = filterArr.filter(num => num > 10);
+console.log(result);
+
+//ForEach Start
+let forArr = [54, 2, 5, 18, 45, 95, 38];
+forArr.forEach(el => console.log(el));
+//ForEach end
+
+//Includes Start
+let incArr = [54, 2, 5, 18, 45, 95, 38];
+console.log(incArr.includes(18));
+//Includes End
+
+//Join Start
+let joArr = [54, 2, 5, 18, 45, 95, 38];
+console.log(joArr.join(' + ' ));
+//Join End
+
+//Reduce Start
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer, 5));
+// expected output: 15
+//Reduce End
+
+//Slice Start
+let sliceArr = ['Vasya', 'Kolya', 'Petya', 'Yura', 'Ihor'];
+console.log(sliceArr.slice(2));
+console.log(sliceArr.slice(2, 4));
--- BasicJS/string/script.js
+++ BasicJS/string/script.js
@@ -16,8 +16,22 @@ console.log(newMessage.length);
 console.log(newMessage.toUpperCase());
 console.log(newMessage.toLowerCase());
 console.log(newMessage.charAt(0));
-console.log(newMessage.indexOf('world'));
+console.log('IndexOf - ', newMessage.indexOf('world'));
 console.log(newMessage.indexOf('cat'));
 console.log(newMessage.substr(1, 4));
 console.log(newMessage.substr(newMessage.indexOf('world'), 5));
 console.log(newMessage.substring(1, 3));
+console.log(newMessage.concat('I\'m a programmer!'));
+console.log('EndsWith - ', newMessage.endsWith('world!!!'));
+console.log('StartsWith - ', newMessage.startsWith('Hello'));
+console.log('Includes - ', newMessage.includes('Hello'));
+console.log('Slice - ', newMessage.slice(1, -3));
+console.log('Split - ', newMessage.split(' '));
+console.log('Trim -', '   Hello   '.trim());
+
+// let result;
+// for (let i = 0; i <= newMessage.length - 1; i++) {
+//     result = newMessage.charAt(i);
+//     console.log(result);
+// };
+
